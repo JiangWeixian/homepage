00ab2f400aba0d42a90edb0302a0601f51a623c5
jiangWeixian <jiangweixian1994@gmail.com>
2022-05-29 23:34:42 +0800

    :sparkles:  revalidate api

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,10 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+export default async function handler(req: NextApiRequest, res: NextApiResponse<string>) {
+  // auth revalidate user with github_token
+  const secret = process.env.GITHUB_TOKEN
+  const authtoken = req.headers['x-auth-token']
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' })
+  if (authtoken === secret) {
+    const issueNumber = req.query.issue_number
+
+    // issue opened or edited
+    // TODO: event should controled by github workflows
+    console.log('[Next.js] Revalidating /issues')
+    await res.unstable_revalidate('/issues')
+    if (issueNumber) {
+      console.log(`[Next.js] Revalidating /issues/id/${issueNumber}`)
+      await res.unstable_revalidate(`/issues/id/${issueNumber}`)
+    }
+
+    return res.status(200).send('Success!')
+  } else {
+    return res.status(403).send('Forbidden')
+  }
 }
