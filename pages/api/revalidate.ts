fee4321a30d0a7b3997e20d66ee4271678657871
JW <jiangweixian1994@gmail.com>
2022-09-29 00:39:33 +0800

    :loud_sound:  log error (#29)

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -6,19 +6,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const secret = process.env.GITHUB_TOKEN
   const authtoken = req.headers['x-auth-token']
 
-  if (authtoken === secret) {
-    const issueNumber = req.query.issue_number
-
-    // issue opened or edited
-    console.log('[Next.js] Revalidating /issues')
-    await res.unstable_revalidate('/issues')
-    if (issueNumber) {
-      console.log(`[Next.js] Revalidating /issues/id/${issueNumber}`)
-      await res.unstable_revalidate(`/issues/id/${issueNumber}`)
+  try {
+    if (authtoken === secret) {
+      const issueNumber = req.query.issue_number
+      // issue opened or edited
+      console.log('[Next.js] Revalidating /issues')
+      await res.unstable_revalidate('/issues')
+      if (issueNumber) {
+        console.log(`[Next.js] Revalidating /issues/id/${issueNumber}`)
+        await res.unstable_revalidate(`/issues/id/${issueNumber}`)
+      }
+      return res.status(200).send('Success!')
+    } else {
+      return res.status(403).send('Forbidden')
     }
-
-    return res.status(200).send('Success!')
-  } else {
-    return res.status(403).send('Forbidden')
+  } catch (e) {
+    console.error('Internal Error', e)
+    return res.status(500).send('Forbidden')
   }
 }
