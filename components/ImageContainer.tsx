b10670417d8e9f2c9a85fb0750f6e662de06e32f
JW <jiangweixian1994@gmail.com>
2024-03-31 22:23:21 +0800

    :bug:  duplicate view-transition-name

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -1,7 +1,8 @@
 import { styled } from 'mayumi/theme'
-import React from 'react'
+import type React from 'react'
 import useMeasure from 'react-use-measure'
-import NextImage, { ImageProps } from 'next/image'
+import type { ImageProps } from 'next/image'
+import NextImage from 'next/image'
 import { withQuery } from 'ufo'
 
 export const ImageContainer = styled('div', {
@@ -23,12 +24,13 @@ const styles: React.CSSProperties = {
   aspectRatio: '2/1',
 }
 
-const RealmeSvgImage = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+const RealmeSvgImage = (props: React.ImgHTMLAttributes<HTMLImageElement> & { imgStyle?: React.ImgHTMLAttributes<HTMLImageElement>['style'] }) => {
   const [ref, bounds] = useMeasure()
   return (
     <div ref={ref} style={{ ...styles, ...props.style }}>
       <img
         {...props}
+        style={props.imgStyle}
         src={withQuery(props.src as string, {
           width: bounds.width.toString(),
           height: bounds.height.toString(),
@@ -40,23 +42,27 @@ const RealmeSvgImage = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
 
 export const Image = (
   { fallbackImgElement = 'next', number, ...props }: ImageProps &
-    React.ImgHTMLAttributes<HTMLImageElement> & { fallbackImgElement?: 'next' | 'raw'; number: number | string },
+  React.ImgHTMLAttributes<HTMLImageElement> & { fallbackImgElement?: 'next' | 'raw'; number: number | string },
 ) => {
   const useViewTransition = typeof number !== 'undefined'
-  const style = useViewTransition 
+  const style = useViewTransition
     ? { viewTransitionName: `cover-${number}`, contain: 'layout' } as any
     : {}
   return (
     <>
-      {typeof props.src === 'string' && !props.src.includes('https://realme') ? (
-        fallbackImgElement === 'next' ? (
+      {typeof props.src === 'string' && !props.src.includes('https://realme')
+        ? (
+            fallbackImgElement === 'next'
+              ? (
           <NextImage style={style} src={props.src} alt={props.alt} objectFit="cover" layout="fill" />
-        ) : (
+                )
+              : (
           <img {...props} style={style} />
-        )
-      ) : (
-        <RealmeSvgImage {...props} src={props.src as string} alt={props.alt} style={style} className="object-fill w-full" />
-      )}
+                )
+          )
+        : (
+        <RealmeSvgImage {...props} src={props.src as string} alt={props.alt} imgStyle={style} className="w-full object-fill" />
+          )}
     </>
   )
 }
