5ce8ca04546b8eb7a62e5fbd8446d600804c1d2d
JW <jiangweixian1994@gmail.com>
2024-04-05 11:29:18 +0800

    :heavy_minus_sign:  prism

--- pages/api/readlist.ts
+++ pages/api/readlist.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from 'lib/prisma'
 
 // POST /api/readlists create record in schema articles
 // Required fields in body: title, url, description
@@ -7,39 +6,10 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { title, url, description } = req.body
   const secret = process.env.GITHUB_TOKEN
   const authtoken = req.headers['x-auth-token']
   if (authtoken !== secret) {
     return res.status(403).end('x-auth-token required')
   }
-  if (req.method === 'POST') {
-    // create or update exit one
-    const result = await prisma.article.upsert({
-      update: {
-        title,
-        url,
-        description,
-      },
-      create: {
-        title,
-        url,
-        description,
-      },
-      where: {
-        url,
-      },
-    })
-    await res.revalidate('/readlist')
-    return res.status(200).json(result)
-  }
-  if (req.method === 'GET') {
-    const result = await prisma.article.findMany({
-      orderBy: [{
-        createdAt: 'desc',
-      }],
-    })
-    return res.status(200).json(result)
-  }
-  return res.status(405).end('Method Not Allowed')
+  await res.revalidate('/readlist')
 }
